d7f97fb08db16b351d9f9d18c585101f87366b3d
Jeffrey <jeff.1001@live.com>
2022-07-24 13:39:43 -0500

    added buttons

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -31,26 +31,8 @@ export default function Recipes(props) {
               {recipeCards}
          </Row>
        </Container>
+       <Button className='create-buttons' variant="primary" style={{float: 'right', marginRight: '7px'}} disabled>Create Recipe</Button>
   </div>
  )       
 }
 
-
-// export default function Recipes({ recipes }) {
-//  const display = recipes.map((p, i) => {
-//      return <li key={i}>{p}</li>
-//  })
-//  return (
-//      <div>
-//          <div className="packages">
-//              <div className="packagesHeader">
-//                  <h1>Our Recipes</h1>
-//                  <ul>
-//                      {display}
-//                  </ul>
-//              </div>
-             
-//          </div>
-//      </div>
-//  )
-// }
