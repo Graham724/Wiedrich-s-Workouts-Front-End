7e8aa45d3588dc768fdc7bdbd87c24721d5ba883
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-07-24 12:19:36 -0500

    added form wireframes

--- src/Components/CreateWorkout.js
+++ src/Components/CreateWorkout.js
@@ -1,10 +1,31 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import '../form.css'
+
 export default function CreateWorkout () {
-    return(
-        <div className="createWorkout">
-            <h1>CreateWorkout</h1>
-   
-   
-        </div>
-    )
+    return (
+        <Form>
+          <Form.Group className="mb-3" controlId="formWorkoutName">
+            <Form.Label>Workout Name</Form.Label>
+            <Form.Control type="recipeName" placeholder="Enter Workout Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formImage">
+            <Form.Label>Image</Form.Label>
+            <Form.Control type="imageName" placeholder="Enter Image URL" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="description" placeholder="Enter Description" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formsteps">
+            <Form.Label>Steps</Form.Label>
+            <Form.Control type="steps" placeholder="Enter Steps" />
+          </Form.Group>
+          
+          <Button variant="primary" type="submit">
+            Add Workout
+          </Button>
+        </Form>
+      );
    
    }
