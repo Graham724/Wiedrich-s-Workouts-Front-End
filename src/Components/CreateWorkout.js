2537173be697342c0a02890b6ea2346fa7b7c2f1
Jeffrey <jeff.1001@live.com>
2022-07-26 18:22:55 -0500

    updated forms

--- src/Components/CreateWorkout.js
+++ src/Components/CreateWorkout.js
@@ -7,7 +7,7 @@ export default function CreateWorkout () {
         <Form>
           <Form.Group className="mb-3" controlId="formWorkoutName">
             <Form.Label>Workout Name</Form.Label>
-            <Form.Control type="recipeName" required="required" placeholder="Enter Workout Name" />
+            <Form.Control type="recipeName" required="required" placeholder="Enter Workout Name" maxLength={50}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formImage">
             <Form.Label>Image</Form.Label>
@@ -15,7 +15,22 @@ export default function CreateWorkout () {
           </Form.Group>
           <Form.Group className="mb-3" controlId="formDescription">
             <Form.Label>Description</Form.Label>
-            <Form.Control type="description" placeholder="Enter Description" />
+            <Form.Control type="description" placeholder="Enter Description" maxLength={50}/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formTime">
+            <Form.Label>Estimated Time</Form.Label>
+            <Form.Control type="number" placeholder="Enter Estimated Time" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Workout Type</Form.Label>
+            <Form.Select >
+              <option>Upper Body Strength</option>
+              <option>Lower Body Strength</option>
+              <option>Core Workout</option>
+              <option>Flexibilty/Recovery</option>
+              <option>Yoga</option>
+              <option>Meditation</option>
+            </Form.Select>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formsteps">
             <Form.Label>Steps</Form.Label>
