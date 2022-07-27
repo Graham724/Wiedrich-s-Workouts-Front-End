d88b2197490f6156baeceb5100c74691bdf15eda
Jeffrey <jeff.1001@live.com>
2022-07-26 21:23:49 -0500

    recipes render with backend connection

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,3 +1,7 @@
+//React features & hooks
+import { useEffect, useState } from 'react'
+
+//React-Bootstrap components
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
@@ -7,25 +11,38 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 
 export default function Recipes(props) {
- const recipes = props.recipes.map((eachRecipe) => {
+const [recipes, setRecipes] = useState([])
+     
+     useEffect(() => {
+          const fetchData = async () => {
+               console.log(process.env)
+               const URL = `${process.env.REACT_APP_BACKEND_URL}/api/recipes`
+               const response = await fetch(URL)
+               const data = await response.json()
+               setRecipes(data)
+          }
+          fetchData()
+     }, [])
+
+ const display = recipes && recipes.map((eachRecipe) => {
   return (
      <Col xs={12} md={4} sm={6} lg={4} className='p-2'>
           <CardGroup>
           <Card>
                <Card.Header>
                     <Card.Title>
-                         {eachRecipe.Title}
+                         {eachRecipe.title}
                     </Card.Title>
                </Card.Header>
-               <Card.Img alt='Food' variant='bottom' src={eachRecipe.Img} />
+               <Card.Img alt='Food' variant='bottom' src={eachRecipe.imgURL} />
                <ListGroup className="list-group-flush">
-                    <ListGroup.Item><span style={{fontWeight: 'bold'}}>Servings: </span>{eachRecipe.Servings}</ListGroup.Item>
-                    <ListGroup.Item><span style={{fontWeight: 'bold'}}>Prep Time: </span>{eachRecipe.PrepTime} minutes</ListGroup.Item>
-                    <ListGroup.Item><span style={{fontWeight: 'bold'}}>Cook Time: </span>{eachRecipe.CookTime} minutes</ListGroup.Item>
+                    <ListGroup.Item><span style={{fontWeight: 'bold'}}>Servings: </span>{eachRecipe.serving}</ListGroup.Item>
+                    <ListGroup.Item><span style={{fontWeight: 'bold'}}>Prep Time: </span>{eachRecipe.prepTime} minutes</ListGroup.Item>
+                    <ListGroup.Item><span style={{fontWeight: 'bold'}}>Cook Time: </span>{eachRecipe.cookTime} minutes</ListGroup.Item>
                </ListGroup>
                <Card.Body>
                     <Card.Text>
-                    <span style={{fontWeight: 'bold'}}>Description: </span> {eachRecipe.Description}
+                    <span style={{fontWeight: 'bold'}}>Description: </span> {eachRecipe.desc}
                     </Card.Text>
                </Card.Body>
                <Card.Footer>
@@ -42,7 +59,7 @@ export default function Recipes(props) {
   <div>
        <h1>Nutritious Recipes!</h1>
           <Row>
-               {recipes}
+               {display}
           </Row>
        <Button className='create-buttons' variant="primary" style={{float: 'right', marginRight: '7px'}}>Create Recipe</Button>
   </div>
