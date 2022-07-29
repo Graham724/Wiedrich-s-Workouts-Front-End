bfccf8030f737669474e7c083e2811364923ea44
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-07-28 19:55:17 -0500

    added homepage info

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,14 +11,12 @@ export default function Home () {
       <div className='homepage_text'>
        <h2>Why We Do It</h2>
        <p>
-       Bro ipsum dolor sit amet hardtail gondy trail swag endo. Stoked shuttle dust on crust, crank bonk bail free ride rail schwag bro single track clipless mute crunchy. Switch berm death cookies acro air shuttle flow. Hammer titanium table top rail. Nose press dirtbag chain suck, chowder face plant death cookies butter stunt bunny slope huck bomb hole dirt.
+       WIEDRICHS-WORKOUTS is a place where users can choose from unique workouts and recipes to jumpstart thier health and wellness journey. 
+       Users can personalize thier wellness by adding, updating, and deleting workouts and recipes.
+       We hope this can help others have a place to share some of their favorite ways to stay fit and healthy, with something for everyone no matter what level of experience you may be at!
+       Theres no better feeling than finding your new favorite recipe or exercise!
        </p>
-       <p>
-       Titanium brain bucket whip 180 beater, Snowboard hurl carcass bunny slope clipless rock roll wheelie drop line sharkbite couloir cruiser. Chillax crunchy bonk titanium Snowboard stoked huck free ride, chowder Bike bomb clean. Chain ring poaching ripper granny gear. Bear trap derailleur presta, rip core shot death cookies chain ring clipless bonk death cookies hurl carcass bro jib trucks euro. Mute north shore travel back country punter scream pillow popping roadie, euro spin backside backside.
-       </p>
-       <p>
-       Bump brain bucket face shots berm frontside bear trap mute grunt dust on crust ollie trucks. OTB schwag gapers, death cookies grom ollie saddle bump stoked. White room couloir schwag, frontside line schwag grind Skate. Yard sale face plant schwag crank, switch pinner nose press gnar huckfest steed rig huckfest.
-       </p>
+       
       </div>
      </div>
  )
